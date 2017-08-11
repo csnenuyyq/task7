636bda610c693867baa0673435c68caf497d9117
csyyq <609882520@qq.com>
2017-08-11 02:52:09 +0000

    　first

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -14,18 +14,37 @@ describe("测试描述", function(){
 
     it("测试用例1", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(6);
+        var expect_string = '6 bottles of beer on the wall, 6 bottles of beer.\n'+
+'Take one down and pass it around, 5 bottles of beer on the wall.\n'+
+'5 bottles of beer on the wall, 5 bottles of beer.\n'+
+'Take one down and pass it around, 4 bottles of beer on the wall.\n'+
+'4 bottles of beer on the wall, 4 bottles of beer.\n'+
+'Take one down and pass it around, 3 bottles of beer on the wall.\n'+
+'3 bottles of beer on the wall, 3 bottles of beer.\n'+
+'Take one down and pass it around, 2 bottles of beer on the wall.\n'+
+'2 bottles of beer on the wall, 2 bottles of beer.\n'+
+'Take one down and pass it around, 1 bottle of beer on the wall.\n'+
+'1 bottle of beer on the wall, 1 bottle of beer.\n'+
+'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+'No more bottles of beer on the wall, no more bottles of beer.\n'+
+'Go to the store and buy some more, 6 bottles of beer on the wall.\n';
         
         expect(expect_string).to.equal(result);
     });
 
     it("测试用例2", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
+//        main();
+        var result = main(3);
+        var expect_string = '3 bottles of beer on the wall, 3 bottles of beer.\n'+
+'Take one down and pass it around, 2 bottles of beer on the wall.\n'+
+'2 bottles of beer on the wall, 2 bottles of beer.\n'+
+'Take one down and pass it around, 1 bottle of beer on the wall.\n'+
+'1 bottle of beer on the wall, 1 bottle of beer.\n'+
+'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+'No more bottles of beer on the wall, no more bottles of beer.\n'+
+'Go to the store and buy some more, 3 bottles of beer on the wall.\n';
         expect(expect_string).to.equal(result);
     });
 });
